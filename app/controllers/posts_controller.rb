694790f2f522a314a2f0c282371b934043e423e9
Thermiee <navyblack16@yahoo.com>
2022-06-10 23:52:28 +0100

    add post contoller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    puts params
   end
 
   def show
-    @post = Post.find(params[:id])
+    puts params
   end
 end
