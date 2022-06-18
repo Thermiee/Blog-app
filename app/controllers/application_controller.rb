5678a5cb80fa417419fc2973f9b81fae5ebb68b6
Thermiee <navyblack16@yahoo.com>
2022-06-18 13:12:40 +0100

    add comments controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  include Response
+  include ExceptionHandler
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,5 +11,19 @@ class ApplicationController < ActionController::Base
       :sign_up,
       keys: %i[name email bio password password_confirmation]
     )
+  end
+end
+
+def authorize_request
+  header = request.headers['Authorization']
+  header = header.split.last if header
+  begin
+    @decoded = JsonWebToken.decode(header)
+    p @decoded
+    @current_user = User.find(@decoded[:user_id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { errors: e.message }, status: :unauthorized
+  rescue JWT::DecodeError => e
+    render json: { errors: e.message }, status: :unauthorized
   end
 end
